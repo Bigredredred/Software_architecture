e9928f099daa6968d738036d04ac329c1728acea
1853582 <642335434@qq.com>
2020-11-14 23:08:22 +0800

    剧情示例

--- src/tongji/edu/Main.java
+++ src/tongji/edu/Main.java
@@ -3,5 +3,9 @@ package tongji.edu;
 public class Main {
     public static void main(String[] args) {
         //大家的剧情代码可以写在这里
+        System.out.println("查理来到庄严雄伟的巧克力工厂门口");
+        System.out.println("门口的威利旺卡已经等候多时了");
+        System.out.println("威利旺卡：欢迎参观我的巧克力工厂！");
+        System.out.println("...");
     }
 }
