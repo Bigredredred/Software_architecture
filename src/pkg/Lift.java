92fa264529fb5e2dce24971e41288a60163655cf
zatnr <1586892962@qq.com>
2020-11-19 18:56:42 +0800

    新建LDL的包

--- src/pkg/Lift.java
+++ src/pkg/Lift.java
@@ -1,6 +1,7 @@
 package pkg;
 
 public class Lift implements Scene, Move{
+    private static Lift theLift = new Lift();
     @Override
     public void welcome() {
         System.out.println("Welcome to Lift, where would you like to go?");
@@ -8,5 +9,9 @@ public class Lift implements Scene, Move{
     public void move(Scene newScene)
     {
         newScene.welcome();
+    }
+
+    public static Lift getInstance(){
+        return theLift;
     }
 }
