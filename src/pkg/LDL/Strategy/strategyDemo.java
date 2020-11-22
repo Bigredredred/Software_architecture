7079b39f8a7335c3dd1535ff8d76daefa58eb4bd
zatnr <1586892962@qq.com>
2020-11-22 15:25:02 +0800

    Demo里介绍巧克力

--- src/pkg/LDL/Strategy/strategyDemo.java
+++ src/pkg/LDL/Strategy/strategyDemo.java
@@ -12,4 +12,15 @@ public class strategyDemo {
         //选择了做夹心巧克力的策略，然后实施该策略，进行巧克力的制作
         contextStuffed.doStrategy("stuffed chocolate");
     }
+//    public static void main(String[] args){
+//        strategyContext contextBlack=new strategyContext(new BlackChocolate());
+//        //选择了做黑巧克力的策略，然后实施该策略，进行巧克力的制作
+//        contextBlack.doStrategy("black chocolate");
+//        strategyContext contextWhite=new strategyContext(new WhiteChocolate());
+//        //选择了做白巧克力的策略，然后实施该策略，进行巧克力的制作
+//        contextWhite.doStrategy("white chocolate");
+//        strategyContext contextStuffed=new strategyContext(new StuffedChocolate());
+//        //选择了做夹心巧克力的策略，然后实施该策略，进行巧克力的制作
+//        contextStuffed.doStrategy("stuffed chocolate");
+//    }
 }
